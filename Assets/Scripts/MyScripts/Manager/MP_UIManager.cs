0532bc25f954684129d7e4ac6ea8af13133c57fe
Laetitia Bourges <laetitia.bourges@etu.univ-amu.fr>
2020-12-18 16:40:39 +0100

    update

--- Assets/Scripts/MyScripts/Manager/MP_UIManager.cs
+++ Assets/Scripts/MyScripts/Manager/MP_UIManager.cs
@@ -7,10 +7,12 @@ public class MP_UIManager : MP_Singleton<MP_UIManager>
 {
     [SerializeField] Button constantQuitButton = null, quitButon = null, restartButton = null, playButton = null;
     [SerializeField] TMP_Text marioPlayerName = null, luigiPlayerName = null, winnerTextBox = null;
+    [SerializeField] TMP_InputField marioInputField = null, luigiInputField = null;
     [SerializeField] GameObject mainMenu = null, winnerMenu = null;
+    bool canMarioStart = false, canLuigiStart = false;
 
     public bool IsUIValid => constantQuitButton && quitButon && restartButton && playButton && marioPlayerName && luigiPlayerName && winnerTextBox
-        && mainMenu && winnerMenu;
+        && mainMenu && winnerMenu && marioInputField && luigiInputField;
 
     private void Start()
     {
@@ -23,6 +25,8 @@ public class MP_UIManager : MP_Singleton<MP_UIManager>
         quitButon.onClick.AddListener(QuitGame);
         restartButton.onClick.AddListener(RestartGame);
         playButton.onClick.AddListener(PlayGame);
+        marioInputField.onEndEdit.AddListener((s) => LockInput(marioInputField, ref canMarioStart));
+        luigiInputField.onEndEdit.AddListener((s) => LockInput(luigiInputField, ref canLuigiStart));
     }
     void QuitGame() => Application.Quit();
     void RestartGame()
@@ -32,8 +36,7 @@ public class MP_UIManager : MP_Singleton<MP_UIManager>
     }
     void PlayGame()
     {
-        if (string.IsNullOrEmpty(marioPlayerName.text) || string.IsNullOrEmpty(luigiPlayerName.text)) return;
-        Debug.Log("test");
+        if (!canMarioStart || !canLuigiStart) return;
         MP_GameManager.Instance?.SetPlayers(new List<string>() { marioPlayerName.text, luigiPlayerName.text });
         MP_GameManager.Instance?.StartGame();
         mainMenu.SetActive(false);
@@ -43,5 +46,12 @@ public class MP_UIManager : MP_Singleton<MP_UIManager>
     {
         winnerTextBox.text = $"{_winnerName} won this party !!";
         winnerMenu.SetActive(true);
+    }
+    void LockInput(TMP_InputField input, ref bool _canStart)
+    {
+        if (input.text.Length > 0)
+            _canStart = true;
+        else if (input.text.Length == 0)
+            _canStart = false;
     }
 }
